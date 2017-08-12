ab9e1f4934198a26a3b67f5bc199952c3f64bd66
wynnfarm <wynnfarm@gmail.com>
2017-08-11 19:08:44 -0500

    Able to view and edit bills

--- app/components/bill-item.js
+++ app/components/bill-item.js
@@ -3,10 +3,22 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   bills: null,
   expanded: false,
+  viewable: true,
 
   actions:{
     toggle(){
       this.toggle('expanded');
+    },
+    edit(){
+      return this.set('viewable', false);
+    },
+    cancel(){
+      return this.set('viewable', true);
+    },
+    save(){
+      debugger;
+      return this.set('viewable', true);
+
     }
   }
 
